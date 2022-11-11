eaee0790857152541fe339929115e7e7b6bf437c
Chamara-Wijepala <mr.worf777@gmail.com>
2022-11-11 18:06:41 +0530

    Add firebase

---
+++ src/firebase-config.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTQG9h_lLQ3pMyZdJ1CaqPaiwKGXDpMxs",
+  authDomain: "comments-system-v2.firebaseapp.com",
+  projectId: "comments-system-v2",
+  storageBucket: "comments-system-v2.appspot.com",
+  messagingSenderId: "1010482457890",
+  appId: "1:1010482457890:web:b707d59ecf828d2be4c97b",
+};
+
+const app = initializeApp(firebaseConfig);
