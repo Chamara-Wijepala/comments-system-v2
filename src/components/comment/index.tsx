adedbe8f1bf4665ee6496af62bd0e3351f5878c6
Chamara-Wijepala <mr.worf777@gmail.com>
2022-11-15 17:21:03 +0530

    Render data received by Comment component

--- src/components/comment/index.tsx
+++ src/components/comment/index.tsx
@@ -1,5 +1,44 @@
 import { IComment } from "types";
 
+import "./style.css";
+
 export default function Comment({ comment }: { comment: IComment }) {
-  return <div>{comment.body}</div>;
+  return (
+    <div className="comment | bg-primary-100 m-block-1 flex flex-column gap-1">
+      <div className="flex flex-align-center gap-1">
+        <img src={comment.photo} alt="" className="comment__image" />
+
+        <div className="flex flex-wrap gap-1">
+          <p className="fw-bold">{comment.userName}</p>
+
+          <p>{comment.createdAt.seconds}</p>
+        </div>
+      </div>
+
+      <p className="comment__body">{comment.body}</p>
+
+      <div className="flex gap-1">
+        <button
+          type="button"
+          className="btn-xs btn--accent border border--accent"
+        >
+          Reply
+        </button>
+
+        <button
+          type="button"
+          className="btn-xs btn--accent border border--accent"
+        >
+          Edit
+        </button>
+
+        <button
+          type="button"
+          className="comment__delete-btn | btn-xs btn--accent border border--accent"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }
